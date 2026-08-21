1d922c09c299c6f1fc712bcabbe83b62968a2096
PriettoDev <284988517+PriettoDev@users.noreply.github.com>
2026-08-21 11:56:27 -0300

    atualização da pilha.h

--- pilha.h
+++ pilha.h
@@ -12,8 +12,23 @@ typedef struct{
 } tp_pilha;
 
 void inicializaPilha(tp_pilha *p){
-
+    p->topo = -1;
 }
+
+int  pilhaVazia(tp_pilha *p){
+    if (p->topo == -1) return 1;
+    return 0;
+}
+
+int pilhaCheia(tp_pilha *p){
+    if (p->topo == MAX-1){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+
 
 
 #endif
