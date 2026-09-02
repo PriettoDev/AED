a0f57e3c688846e3fdad3a5d66cfb86c8fd18287
PriettoDev <284988517+PriettoDev@users.noreply.github.com>
2026-09-02 11:23:05 -0300

    Feat: Atualização da biblioteca fila.h, com a adição das novas funções.

--- Queue/fila.h
+++ Queue/fila.h
@@ -16,4 +16,62 @@ void inicializaFila(tp_fila *f){
     //f->tam=0;
 }
 
+int filaVazia (tp_fila *f){
+    if(f->ini == f->fim){
+        return 1;
+    }
+    return 0;
+}
+
+int proximo(int pos){
+    if(pos == MAX-1) return 0;
+    return ++pos;
+}
+
+int filaCheia(tp_fila *f){
+    if(proximo(f->fim)==f->ini){
+        return 1;
+    }
+    return 0;
+}
+
+int insereFila(tp_fila *f, tp_item e){
+    if(filaCheia(f)){
+        return 0; //Não foi possivel adicionar a fila
+    }
+
+    f->fim = proximo(f->fim);
+    f->item[f->fim] = e;
+    // f->tam++;
+    return 1;
+}
+
+int removeFila(tp_fila *f, tp_item *e){
+    if(filaVazia(f)){
+        return 0; //Não foi possivel remover da fila
+    }
+    f->ini = proximo(f->ini);
+    *e = f->item[f->ini];
+    //f->tam--;
+    return 1;
+}
+
+void imprimeFila(tp_fila f){
+    tp_item e;
+    while (!filaVazia(&f)){
+        removeFila(&f, &e);
+        printf("\n%d", e);
+    }
+}
+
+int tamanhoFila(tp_fila *f){
+    if(filaVazia(f)){
+        return 0;
+    }
+    if(f->ini < f->fim){
+        return f->fim - f->ini;
+    }
+    return MAX-1 - f->ini + f->fim+1;
+}
+
 #endif
